937ce295468cf1bbad42c66e012bfbef42c5e83a
Vivek Kumar <vk.aries@gmail.com>
2024-02-04 16:14:25 +0530

    update reordering code

--- lec08/ordering.cpp
+++ lec08/ordering.cpp
@@ -5,7 +5,6 @@
  */
 
 #include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <random>
@@ -25,87 +24,111 @@ inline void wait_random() {
 //-------------------------------------
 //  Main program, as decribed in the post
 //-------------------------------------
-sem_t beginSema1;
-sem_t beginSema2;
-sem_t endSema;
+pthread_mutex_t m1=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m2=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m=PTHREAD_MUTEX_INITIALIZER;
+
+volatile int counter1 = 0;
+volatile int counter2 = 0;
+volatile int counter = 0;
+volatile int shutdown = 0;
+
+pthread_cond_t c1=PTHREAD_COND_INITIALIZER;
+pthread_cond_t c2=PTHREAD_COND_INITIALIZER;
+pthread_cond_t c=PTHREAD_COND_INITIALIZER;
+
+void wait(int id) {
+  switch(id) {
+    case 0:
+      pthread_mutex_lock(&m);
+      while(counter != 2) {
+        pthread_cond_wait(&c, &m);
+      }
+      counter=0;
+      pthread_mutex_unlock(&m);
+      break;
+    case 1:
+      pthread_mutex_lock(&m1);
+      while(counter1 == 0) {
+        pthread_cond_wait(&c1, &m1);
+      }
+      counter1=0;
+      pthread_mutex_unlock(&m1);
+      break;
+    case 2:
+      pthread_mutex_lock(&m2);
+      while(counter2 == 0) {
+        pthread_cond_wait(&c2, &m2);
+      }
+      counter2=0;
+      pthread_mutex_unlock(&m2);
+      break;
+  }
+  wait_random();
+}
+
+void signal(int id) {
+  switch(id) {
+    case 0:
+      pthread_mutex_lock(&m);
+      counter++;
+      pthread_cond_signal(&c);
+      pthread_mutex_unlock(&m);
+      break;
+    case 1:
+      pthread_mutex_lock(&m1);
+      counter1=1;
+      pthread_cond_signal(&c1);
+      pthread_mutex_unlock(&m1);
+      break;
+    case 2:
+      pthread_mutex_lock(&m2);
+      counter2=1;
+      pthread_cond_signal(&c2);
+      pthread_mutex_unlock(&m2);
+      break;
+  }
+}
 
 int X, Y;
 int r1, r2;
 
-void *threadFunc(void *param)
-{
-    for (;;)
-    {
-        sem_wait(&beginSema1);  // Wait for signal
-        wait_random();
-
-        // ----- THE TRANSACTION! -----
-        X = 1;
+void *threadFunc(void *param) {
+  int id = *((int*)param);
+  while(!shutdown) {
+    wait(id);
+    switch(id) {
+      case 1:
+	X = 1;
 #ifdef USE_CPU_FENCE
         asm volatile("mfence" ::: "memory");  // Prevent CPU reordering
 #else
         asm volatile("" ::: "memory");  // Prevent compiler reordering
 #endif
         r1 = Y;
-
-        sem_post(&endSema);  // Notify transaction complete
-    }
-    return NULL;  // Never returns
-};
-
-void *thread1Func(void *param)
-{
-    for (;;)
-    {
-        sem_wait(&beginSema1);  // Wait for signal
-	wait_random();
-
-        // ----- THE TRANSACTION! -----
-        X = 1;
-#ifdef USE_CPU_FENCE
-        asm volatile("mfence" ::: "memory");  // Prevent CPU reordering
-#else
-        asm volatile("" ::: "memory");  // Prevent compiler reordering
-#endif
-        r1 = Y;
-
-        sem_post(&endSema);  // Notify transaction complete
-    }
-    return NULL;  // Never returns
-};
-
-void *thread2Func(void *param)
-{
-    for (;;)
-    {
-        sem_wait(&beginSema2);  // Wait for signal
-        wait_random(); // Random delay
-
-        // ----- THE TRANSACTION! -----
-        Y = 1;
+        break;
+      case 2:
+	Y = 1;
 #ifdef USE_CPU_FENCE
         asm volatile("mfence" ::: "memory");  // Prevent CPU reordering
 #else
         asm volatile("" ::: "memory");  // Prevent compiler reordering
 #endif
         r2 = X;
-
-        sem_post(&endSema);  // Notify transaction complete
+        break;
     }
-    return NULL;  // Never returns
-};
+    signal(0);
+  }
+  return NULL;  // Never returns
+}
 
 int main()
 {
-    // Initialize the semaphores
-    sem_init(&beginSema1, 0, 0);
-    sem_init(&beginSema2, 0, 0);
-    sem_init(&endSema, 0, 0);
-
     // Spawn the threads
     pthread_t thread1, thread2;
-    pthread_create(&thread1, NULL, thread1Func, NULL);
-    pthread_create(&thread2, NULL, thread2Func, NULL);
+    int id1=1, id2=2;
+    pthread_create(&thread1, NULL, threadFunc, ((void*)&id1));
+    pthread_create(&thread2, NULL, threadFunc, ((void*)&id2));
     usleep(500);
 #ifdef USE_SINGLE_HW_THREAD
     // Force thread affinities to the same cpu core.
@@ -124,11 +147,10 @@ int main()
         X = 0;
         Y = 0;
         // Signal both threads
-        sem_post(&beginSema1);
-        sem_post(&beginSema2);
+        signal(1);
+	signal(2);
         // Wait for both threads
-        sem_wait(&endSema);
-        sem_wait(&endSema);
+	wait(0);
         // Check if there was a simultaneous reorder
         if (r1 == 0 && r2 == 0)
         {
@@ -136,5 +158,11 @@ int main()
             printf("%d reorders detected after %d iterations\n", detected, iterations);
         }
     }
+    shutdown=1;
+    // Signal both threads
+    signal(1);
+    signal(2);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
     return 0;  // Never returns
 }
